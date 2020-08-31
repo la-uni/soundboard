4f88055fd7acd754906115e6a8a8e10a2bb7cb66
la-uni <65335086+la-uni@users.noreply.github.com>
2020-08-31 17:42:13 +1000

    Create soundboard.js

---
+++ soundboard.js
@@ -0,0 +1,314 @@
+
+//QWERTYUIOP - line 1
+//immanence becomes imminence the ash of annihilation lands softly on
+//Q
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 81 ) {
+        // document.getElementById('seal_bb2_bay').play()
+        document.getElementById("body").style.backgroundColor = "Violet"
+        document.getElementById("phoneme").innerHTML = "immanence"
+        
+    }
+})
+
+//W
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 87) {
+        // document.getElementById('genie_bb_bay').play()
+        // document.getElementById("body").style.backgroundColor = "Salmon"
+        document.getElementById("phoneme").innerHTML = "becomes"
+    }
+})
+
+//E
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 69) {
+        // document.getElementById('bieber_bb_bay').play()
+        // document.getElementById("body").style.backgroundColor = "Aqua"
+        document.getElementById("phoneme").innerHTML = "imminence"
+    }
+})
+
+//R
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 82) {
+        // document.getElementById('mount_bb_bay').play()
+        // document.getElementById("body").style.backgroundColor = "PeachPuff"
+        document.getElementById("phoneme").innerHTML = "the"
+    }
+})
+
+//T
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 84) {
+        // document.getElementById('warpaint_bb_bay').play()
+        // document.getElementById("body").style.backgroundColor = "DarkOliveGreen"
+        document.getElementById("phoneme").innerHTML = "ash"
+    }
+})
+
+//Y
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 89) {
+        // document.getElementById('seal_bb1_bay').play()
+        // document.getElementById("body").style.backgroundColor = "DarkSlateGrey"
+        document.getElementById("phoneme").innerHTML = "of"
+    }
+})
+
+//U
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 85) {
+        // document.getElementById('ronettes_bb_bay').play()
+        // document.getElementById("body").style.backgroundColor = "Crimson"
+        document.getElementById("phoneme").innerHTML = "annihilation"
+    }
+})
+
+//I
+document.addEventListener ('keydown', function(e) {
+    if (e.keyCode == 73) {
+        // document.getElementById('brit_bb_beh').play()
+        // document.getElementById("body").style.backgroundColor = "Pink"
+        document.getElementById("phoneme").innerHTML = "lands"
+    }
+})
+
+//O
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 79) {
+        // document.getElementById('otzi_bb_bay').play()
+        // document.getElementById("body").style.backgroundColor = "Yellow"
+        document.getElementById("phoneme").innerHTML = "softly"
+    }
+})
+
+//P
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 80) {
+        // document.getElementById('carly_bb_bay').play()
+        // document.getElementById("body").style.backgroundColor = "MediumOrchid"
+        document.getElementById("phoneme").innerHTML = "on"
+    }
+})
+
+
+//ASDFGHJKL - line 2
+//lungs of those watching the sun darkens in its
+
+//A
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 65) {
+        // document.getElementById('seal_bb2_bay').play()
+        document.getElementById("body").style.backgroundColor = "Violet"
+        document.getElementById("phoneme").innerHTML = "lungs"
+    }
+})
+
+//S
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 83) {
+        // document.getElementById('genie_bb_bay').play()
+        // document.getElementById("body").style.backgroundColor = "Salmon"
+        document.getElementById("phoneme").innerHTML = "of"
+
+    }
+})
+
+//D
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 68) {
+        // document.getElementById('bieber_bb_bay').play()
+        // document.getElementById("body").style.backgroundColor = "Aqua"
+        document.getElementById("phoneme").innerHTML = "those"
+    }
+})
+
+//F
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 70) {
+        // document.getElementById('mount_bb_bay').play()
+        // document.getElementById("body").style.backgroundColor = "PeachPuff"
+        document.getElementById("phoneme").innerHTML = "watching"
+    }
+})
+
+//G
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 71) {
+        // document.getElementById('warpaint_bb_bay').play()
+        // document.getElementById("body").style.backgroundColor = "DarkOliveGreen"
+        document.getElementById("phoneme").innerHTML = "the"
+    }
+})
+
+//H
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 72) {
+        // document.getElementById('seal_bb1_bay').play()
+        // document.getElementById("body").style.backgroundColor = "DarkSlateGrey"
+        document.getElementById("phoneme").innerHTML = "sun"
+    }
+})
+
+
+//J
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 74) {
+        // document.getElementById('ronettes_bb_bay').play()
+        // document.getElementById("body").style.backgroundColor = "Crimson"
+        document.getElementById("phoneme").innerHTML = "darkens"
+    }
+})
+
+//K
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 75) {
+        // document.getElementById('brit_bb_bay').play()
+        // document.getElementById("body").style.backgroundColor = "Red"
+        document.getElementById("phoneme").innerHTML = "in"
+    }
+})
+
+//L
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 76) {
+        // document.getElementById('otzi_bb_bay').play()
+        // document.getElementById("body").style.backgroundColor = "Yellow"
+        document.getElementById("phoneme").innerHTML = "its"
+    }
+})
+
+
+//ZXCVBNM - line 3
+//brightness I am already word without end
+
+//Z
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 90) {
+        // document.getElementById('seal_bb2_beh').play()
+        // document.getElementById("body").style.backgroundColor = "SlateBlue"
+        document.getElementById("phoneme").innerHTML = "brightness"
+
+    }
+})
+
+//X
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 88) {
+        // document.getElementById('genie_bb_beh').play()
+        // document.getElementById("body").style.backgroundColor = "Turquoise"
+        document.getElementById("phoneme").innerHTML = "I"
+    }
+})
+
+
+//C
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 67) {
+        // document.getElementById('bieber_bb_beh').play()
+        // document.getElementById("body").style.backgroundColor = "SteelBlue"
+        document.getElementById("phoneme").innerHTML = "am"
+
+    }
+})
+
+//R, F, V - BigMountain_Baby_I_Love_Your_Way
+
+
+
+//V
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 86) {
+        // document.getElementById('mount_bb_beh').play()
+        // document.getElementById("body").style.backgroundColor = "Peru"
+        document.getElementById("phoneme").innerHTML = "already"
+
+    }
+})
+
+//T, G, B - Warpaint_Baby
+
+
+
+
+//B
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 66) {
+        // document.getElementById('warpaint_bb_beh').play()
+        document.getElementById("body").style.backgroundColor = "PaleVioletRed"
+        document.getElementById("phoneme").innerHTML = "word"
+    }
+})
+
+//Y, H, N - Seal_KissFromARose_1
+
+//N
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 78) {
+        // document.getElementById('seal_bb1_beh').play()
+        // document.getElementById("body").style.backgroundColor = "Thistle"
+        document.getElementById("phoneme").innerHTML = "without"
+
+    }
+})
+
+
+//M
+document.addEventListener ('keydown', function(e) {
+    if (e.keyCode == 77) {
+        // document.getElementById('ronettes_bb_beh').play()
+        // document.getElementById("body").style.backgroundColor = "Cyan"
+        document.getElementById("phoneme").innerHTML = "end"
+    }
+})
+
+
+
+// //(,)
+// document.addEventListener ('keydown', function(e) {
+//     if (e.keyCode == 188) {
+//         document.getElementById('brit_bb_beh').play()
+//         document.getElementById("body").style.backgroundColor = "Pink"
+//         document.getElementById("phoneme").innerHTML = "beh"
+//     }
+// })
+//
+//
+// //(.)
+// document.addEventListener ('keydown', function(e) {
+//     if (e.keyCode == 190) {
+//         document.getElementById('otzi_bb_beh').play()
+//         document.getElementById("body").style.backgroundColor = "LimeGreen"
+//         document.getElementById("phoneme").innerHTML = "beh"
+//     }
+// })
+//
+//
+// //(;)
+// document.addEventListener('keydown', function(e) {
+//     if (e.keyCode == 186) {
+//         document.getElementById('carly_bb_bay').play()
+//         document.getElementById("body").style.backgroundColor = "MediumOrchid"
+//         document.getElementById("phoneme").innerHTML = "bay"
+//     }
+// })
+//
+//
+// //(/)
+// document.addEventListener ('keydown', function(e) {
+//     if (e.keyCode == 191) {
+//         document.getElementById('carly_bb_beh').play()
+//         document.getElementById("body").style.backgroundColor = "MediumPurple"
+//         document.getElementById("phoneme").innerHTML = "beh"
+//     }
+// })
+
+
+// document.addEventListener('keyup', function(e) {
+//     if (e.keyCode == 87) {
+//         document.getElementById('genie_bb_beh').play()
+//         document.getElementById("body").style.backgroundColor = "Turquoise"
+//         document.getElementById("phoneme").innerHTML = "beh"
+//     }
+// })
